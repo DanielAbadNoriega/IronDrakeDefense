060af9d91f70412f9baa85241ad7d4488c7e6933
Idoia Vélez <Idoia2b@iMac-de-Idoia-2.local>
2021-05-09 15:13:23 +0200

    Iteration: Background changed

--- src/dragon.js
+++ src/dragon.js
@@ -23,7 +23,7 @@ class Dragon {
         }
 
         this.img = new Image();
-        this.img.src = './Images/Drake&WolfM.png'
+        this.img.src = './Images/Drake&Wolf.png'
 
         this.img.frames = 4;
         this.img.frameIndex = 0;
